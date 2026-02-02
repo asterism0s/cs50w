9d22d95667ca8e65a802344bed68f12984fb3df8
asterism0s <sarahtrad30@gmail.com>
2026-02-02 15:21:02 +0100

    fix(newyear): fix typo

--- django/lecture3/newyear/views.py
+++ django/lecture3/newyear/views.py
@@ -6,6 +6,6 @@ from django.shortcuts import render
 
 def index(request):
     now = datetime.datetime.now()
-    return render(request, "newuear/index.html", {
+    return render(request, "newyear/index.html", {
         "newyear": now.month == 1 and now.day == 1
     })
