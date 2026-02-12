490bc50f89a20bea32aa2d3d13941b43d524bbec
asterism0s <sarahtrad30@gmail.com>
2026-02-12 14:32:50 +0100

    feat(wiki): add entry lookup logic to entry view

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -9,5 +9,24 @@ def index(request):
         "entries": util.list_entries()
     })
 
-def entry(request):
-    return HttpResponse("Hello")
+def entry(request, title):
+     
+
+    entries = util.list_entries()
+
+    for entry in entries:
+        if entry.lower() == title.lower():
+            show_title = entry
+
+    #pegar o que foi escrito na url e converter pra lower
+    #pegar a entry e converter pra lower
+    #comparar as strings
+    #mandar pra fora
+
+    content = util.get_entry(title)
+    
+    return HttpResponse(show_title)
+
+    # return render (request, "wiki/entry.html", {
+    #     content: title.lower()
+    # })
