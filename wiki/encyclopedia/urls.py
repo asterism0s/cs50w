3f0c49b4e26736eee1a1be4fd8e33ef07c2feff7
asterism0s <sarahtrad30@gmail.com>
2026-02-17 14:17:16 +0100

    fix: rename URL path prefix from 'encyclopedia' to 'wiki'

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("encyclopedia/<str:title>", views.entry, name="title")
+    path("wiki/<str:title>", views.entry, name="title")
 ]
