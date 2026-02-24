c57d28395240afe3cf714eb9e7a4e493da78b1dc
asterism0s <sarahtrad30@gmail.com>
2026-02-24 09:32:59 +0100

    feat(search): add redirect logic for exact match queries

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,9 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from markdown2 import markdown
 
 from . import util
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -26,3 +25,26 @@ def entry(request, title):
             })
         
     return render(request, "encyclopedia/error.html")
+
+def search(request):
+
+    query = request.GET.get('q')
+    entries = util.list_entries()
+
+    for entry in entries:
+        print(entry)
+
+        if query.lower() == entry.lower():
+            return redirect('')
+
+
+    #print(f"Data received: {query}")
+    #print(f"GET Completo: {request.GET}")
+
+    #search recebe os dados do form
+        #SE a consulta corresponder com os dados da entrada, usuário será redirecionado para a página da entrada
+        
+        #SENÃO o usuário deve ser enviado a uma página de resultados que exibe todas as entradas que contém a consuylta como substring.
+
+
+    return HttpResponse("Teste")
