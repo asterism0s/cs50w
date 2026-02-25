447131d0eed8118556734473c843ab1299144cb9
asterism0s <sarahtrad30@gmail.com>
2026-02-25 13:36:50 +0100

    feat(search): render results template with partial matches

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -15,11 +15,12 @@ def entry(request, title):
     for entry in entries:
         
         if entry.lower() == title.lower():
+
             content = util.get_entry(entry)
             content_mk = markdown(content)
-            print(content_mk)
+            
             return render(request, "encyclopedia/entry.html", {
-                                "title": entry,
+                "title": entry,
                 "content": content_mk
             })
         
@@ -30,20 +31,18 @@ def search(request):
     query = request.GET.get('q')
     entries = util.list_entries()
 
+    results = []
+
     for entry in entries:
-        print(entry)
 
         if query.lower() == entry.lower():
             return redirect('title', query)
-
-
-    #print(f"Data received: {query}")
-    #print(f"GET Completo: {request.GET}")
-
-    #search recebe os dados do form
-        #SE a consulta corresponder com os dados da entrada, usuário será redirecionado para a página da entrada
         
-        #SENÃO o usuário deve ser enviado a uma página de resultados que exibe todas as entradas que contém a consuylta como substring.
+        elif query.lower() in entry.lower():
+            results.append(entry)
 
-
-    return HttpResponse("Teste")
+    return render(request, "encyclopedia/results.html", {
+                "title": entry,
+                "query": query,
+                "results": results
+            })
