60e0a8f8d7edc2bc650ac21f0b8bc1635e269104
asterism0s <sarahtrad30@gmail.com>
2026-03-12 04:02:16 -0400

    feat(edit): add URL pattern for edit entry functionality

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="title"),
+    path("wiki/<str:title>/edit", views.edit, name="edit"),
     path("search", views.search, name="search"),
     path("create", views.create, name="create")
 ]
