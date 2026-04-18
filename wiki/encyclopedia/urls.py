2f17f4f34723ebcc2891be3a96d0f2fc892230a9
asterism0s <sarahtrad30@gmail.com>
2026-04-18 13:24:14 +0200

    refactor(urls): standardize URL patterns with wiki/ prefix

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("wiki/search", views.search, name="search"),
+    path("wiki/create", views.create, name="create"),
     path("wiki/<str:title>", views.entry, name="title"),
     path("wiki/<str:title>/edit", views.edit, name="edit"),
-    path("search", views.search, name="search"),
-    path("create", views.create, name="create")
+    
 ]
