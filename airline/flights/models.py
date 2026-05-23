0f2ce9ff13b9496837a8151cbb6bf7d43920b60b
asterism0s <sarahtrad30@gmail.com>
2026-05-23 14:00:06 +0200

    fix(flights): correct IntegerField syntax in Flight model

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Flight(models.Model):
     origin = models.CharField(max_length=64)
     destination = models.CharField(max_length=64)
-    duration = models.Integer.Field()
+    duration = models.IntegerField()
