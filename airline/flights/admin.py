2d9c061a32416806184676482b3cfc329a9afd5c
asterism0s <sarahtrad30@gmail.com>
2026-05-30 16:53:44 +0200

    feat(admin): customize Flight admin interface to display relevant fields

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Flight, Airport, Passenger
 
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")
+
 admin.site.register(Airport)
-admin.site.register(Flight)
+admin.site.register(Flight, FlightAdmin)
 admin.site.register(Passenger)
