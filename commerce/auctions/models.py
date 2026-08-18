ae7da1769f395ef5d416f2f1661872357f2eb10d
asterism0s <sarahtrad30@gmail.com>
2026-08-18 15:26:07 +0200

    refactor(auctions): replace category field with foreign key to Category model

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -5,32 +5,16 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+class Category (models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.name}"
+
 class AuctionListings(models.Model):
-    FASHION = "FA"
-    ELECTRONICS = "EL"
-    FURNITURE = "FU"
-    TOYS = "TO"
-    SPORTS = "SP"
-    BOOKS = "BO"
-    ART = "AR"
-    VEHICLES = "VE"
-    NOT_DEFINED = "ND"
-
-    CATEGORIES_CHOICES = {
-        FASHION: "Fashion",
-        ELECTRONICS: "Electronics",
-        FURNITURE: "Furniture",
-        TOYS: "Toys",
-        SPORTS: "Sports",
-        BOOKS: "Books",
-        ART: "Art",
-        VEHICLES: "Vehicles",
-        NOT_DEFINED: "Not defined"
-    }
-
 
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="created_listings")
-    category = models.CharField(max_length=2, choices=CATEGORIES_CHOICES, default=NOT_DEFINED, blank=True)
+    category = models.ForeignKey(Category, related_name="listings", on_delete=models.SET_NULL, blank=True, null=True)
     creation_date = models.DateTimeField(auto_now_add=True)
     description = models.TextField()
     image = models.URLField(blank=True)
