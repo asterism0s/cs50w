5d24f7d8a9c4cb2027a5ded0d5895d20cb31b868
asterism0s <sarahtrad30@gmail.com>
2026-08-19 16:22:51 +0200

    feat(auctions): register all models in admin.py

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models import AuctionListing, Bid, Comment, Category
+
 # Register your models here.
+class AuctionListingAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "author", "category", "creation_date", "start_bid", "is_active")
+
+class BidAdmin(admin.ModelAdmin):
+    list_display = ("id", "author", "listing", "amount")
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("author", "listing", "comment")
+
+
+admin.site.register(Category)
+admin.site.register(AuctionListing, AuctionListingAdmin)
+admin.site.register(Bid, BidAdmin)
+admin.site.register(Comment, CommentAdmin)
