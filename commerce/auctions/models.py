fd6cba77c9b5f64dce340c22419013cb9c867edc
asterism0s <sarahtrad30@gmail.com>
2026-08-19 15:17:39 +0200

    feat(auctions): add Comment model

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -13,8 +13,8 @@ class Category (models.Model):
 
 class AuctionListing(models.Model):
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="created_listing")
-    category = models.ForeignKey(Category, related_name="listing", on_delete=models.SET_NULL, blank=True, null=True)
+    author = models.ForeignKey(User, related_name="created_listings", on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name="listings", on_delete=models.SET_NULL, blank=True, null=True)
     creation_date = models.DateTimeField(auto_now_add=True)
     description = models.TextField()
     image = models.URLField(blank=True)
@@ -26,12 +26,17 @@ class AuctionListing(models.Model):
         return f"{self.title} ({self.author})"
 
 class Bid(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="created_bid")
-    listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="bid")
+    author = models.ForeignKey(User, related_name="created_bids", on_delete=models.CASCADE)
+    listing = models.ForeignKey(AuctionListing, related_name="bids", on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=8, decimal_places=2)
 
     def __str__(self):
         return f"Bid of {self.amount} on {self.listing} by {self.author}"
 
 class Comment(models.Model):
-    author= models.ForeignKey(User, on_delete=models.CASCADE, related_name="created_comment")
+    author= models.ForeignKey(User, related_name="created_comments", on_delete=models.CASCADE)
+    listing = models.ForeignKey(AuctionListing, related_name="comments", on_delete= models.CASCADE)
+    comment = models.TextField()
+
+    def __str__(self):
+        return f"Comment made by {self.author} on {self.listing}"
